aefecf0c6f3d459644ca5c197abd15e9b19826cd
Bryan Kottama <bryan@kyodo-i.com>
2020-05-02 13:54:34 +0700

    form validation user edit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -123,6 +123,13 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
+        Validator::make($request->all(), [
+            "name" => "required|min:5|max:100",
+            "roles" => "required",
+            "phone" => "required|digits_between:10,12",
+            "address" => "required|min:20|max:200",
+        ])->validate();
+
         $user = User::findOrFail($id);
         $user->name = $request->name;
         $user->roles = json_encode($request->roles);
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -6,63 +6,79 @@ Edit User
 
 @section('content')
 <div class="col-md-8">
-
-    @if (session('status'))
+    @if(session('status'))
     <div class="alert alert-success">
         {{session('status')}}
     </div>
     @endif
-
-    <form action="{{route('users.update', [$user->id])}}" method="PUT" enctype="multipart/form-data"
-        class="bg-white shadow-sm p-3">
+    <form enctype="multipart/form-data" class="bg-white shadow-sm p-3" action="{{route('users.update', [$user->id])}}"
+        method="POST">
         @csrf
         <input type="hidden" value="PUT" name="_method">
-
         <label for="name">Name</label>
-        <input value="{{$user->name}}" class="form-control" placeholder="Full Name" type="text" name="name" id="name" />
-        <br />
+        <input value="{{old('name') ? old('name') : $user->name}}"
+            class="form-control {{$errors->first('name') ? "is-invalid" : ""}}" placeholder="Full Name" type="text"
+            name="name" id="name" />
+
+        <div class="invalid-feedback">
+            {{$errors->first('name')}}
+        </div>
+        <br>
 
         <label for="username">Username</label>
         <input value="{{$user->username}}" disabled class="form-control" placeholder="username" type="text"
             name="username" id="username" />
-        <br />
+        <br>
 
         <label for="">Status</label>
-        <br>
+        <br />
         <input {{$user->status == "ACTIVE" ? "checked" : ""}} value="ACTIVE" type="radio" class="form-control"
-            name="status" id="active">
-        <label for="active">Active</label>
+            id="active" name="status"> <label for="active">Active</label>
 
         <input {{$user->status == "INACTIVE" ? "checked" : ""}} value="INACTIVE" type="radio" class="form-control"
-            name="status" id="inactive">
-        <label for="inactive">Inactive</label>
+            id="inactive" name="status"> <label for="inactive">Inactive</label>
+        <br><br>
 
         <label for="">Roles</label>
-        <br />
+        <br>
         <input type="checkbox" {{in_array("ADMIN", json_decode($user->roles)) ? "checked" : ""}} name="roles[]"
-            id="ADMIN" value="ADMIN" />
+            class="form-control {{$errors->first('roles') ? "is-invalid" : "" }}" id="ADMIN" value="ADMIN">
         <label for="ADMIN">Administrator</label>
 
         <input type="checkbox" {{in_array("STAFF", json_decode($user->roles)) ? "checked" : ""}} name="roles[]"
-            id="STAFF" value="STAFF" />
+            class="form-control {{$errors->first('roles') ? "is-invalid" : "" }}" id="STAFF" value="STAFF">
         <label for="STAFF">Staff</label>
 
         <input type="checkbox" {{in_array("CUSTOMER", json_decode($user->roles)) ? "checked" : ""}} name="roles[]"
-            id="CUSTOMER" value="CUSTOMER" />
+            class="form-control {{$errors->first('roles') ? "is-invalid" : "" }}" id="CUSTOMER" value="CUSTOMER">
         <label for="CUSTOMER">Customer</label>
-        <br />
 
-        <br />
-        <label for="phone">Phone Number</label>
-        <br />
-        <input type="text" name="phone" class="form-control" value="{{$user->phone}}">
-
+        <div class="invalid-feedback">
+            {{$errors->first('roles')}}
+        </div>
         <br>
+        <br>
+
+        <label for="phone">Phone number</label>
+        <br>
+        <input type="text" name="phone" class="form-control {{$errors->first('phone') ? "is-invalid" : ""}}"
+            value="{{old('phone') ? old('phone') : $user->phone}}">
+
+        <div class="invalid-feedback">
+            {{$errors->first('phone')}}
+        </div>
+        <br>
+
         <label for="address">Address</label>
-        <textarea name="address" id="address" class="form-control">{{$user->address}}</textarea>
+        <textarea name="address" id="address"
+            class="form-control {{$errors->first('address') ? "is-invalid" : ""}}">{{old('address') ? old('address') : $user->address}}</textarea>
 
+        <div class="invalid-feedback">
+            {{$errors->first('address')}}
+        </div>
         <br>
-        <label for="avatar">Avatar Image</label>
+
+        <label for="avatar">Avatar image</label>
         <br>
         Current avatar: <br>
         @if($user->avatar)
@@ -72,19 +88,22 @@ Edit User
         No avatar
         @endif
         <br>
-        <input type="file" id="avatar" name="avatar" class="form-control">
+
+        <input id="avatar" name="avatar" type="file" class="form-control">
         <small class="text-muted">Kosongkan jika tidak ingin mengubah
             avatar</small>
 
-        <hr class="my-3">
-
+        <hr class="my-4">
+        
         <label for="email">Email</label>
-        <input type="text" class="form-control" placeholder="user@mail.com" name="email" id="email"
-            value="{{$user->email}}" disabled>
+        <input value="{{$user->email}}" disabled class="form-control {{$errors->first('email') ? "is-invalid" : ""}} "
+            placeholder="user@mail.com" type="text" name="email" id="email" />
+        <div class="invalid-feedback">
+            {{$errors->first('email')}}
+        </div>
         <br>
-
-        <input type="submit" class="btn btn-primary" value="Save">
-
+        
+        <input class="btn btn-primary" type="submit" value="Simpan" />
     </form>
 </div>
 @endsection
